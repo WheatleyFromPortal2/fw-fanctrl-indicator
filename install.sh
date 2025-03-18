b31a93e5ebe8ab3696273de6cf052bacbfc75b2d
Hunter Dorrill <hunterdorrill@icloud.com>
2025-03-18 07:30:30 -0500

    install fix 10

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ read -p "Enter the path for your fw-fanctrl install - ex. /home/YOUR_USER/.confi
 sudo cp fw-fanctrl-indicator.py *.svg $configPath # move our python and icon files to where fw-fanctrl is installed
 sedArg1="s%FW_FANCTRL_INDICATOR%${configPath}/fw-fanctrl-indicator.py%g"
 sedArg2="s%FW_FANCTRL%${configPath}%g"
-sed -e -e ${sedArg1} fw-fanctrl-indicator.service # fingers crossed
+sed -i -e ${sedArg1} fw-fanctrl-indicator.service # fingers crossed
 sed -i -e ${sedArg2} fw-fanctrl-indicator.service # let's hope this works
 
 sudo cp fw-fanctrl-indicator.service /etc/systemd/system/ # copy our systemd service file to where the services are stored
