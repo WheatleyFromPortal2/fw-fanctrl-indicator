8c50fb5f0269ee3b0d6f68d5bb3d77af36f24432
Hunter Dorrill <hunterdorrill@icloud.com>
2025-03-22 05:44:55 -0500

    clean up code and install

--- fw-fanctrl-indicator.py
+++ fw-fanctrl-indicator.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
-# copied from https://fosspost.org/custom-system-tray-icon-indicator-linux with import modified
+# initial code copied from https://fosspost.org/custom-system-tray-icon-indicator-linux with import modified
 # requires libappindicator-gtk3 to be installed (either through your package manager or pip)
 import os, gi, json
 
-configPath = f"{__file__.replace("fw-fanctrl-indicator.py", '')}config.json"
-iconPath = f"{__file__.replace("fw-fanctrl-indicator.py", '')}fan-white.svg"
+configPath = f"{__file__.replace("fw-fanctrl-indicator.py", '')}config.json" # use the location of this python file to find the config (because it should be installed there)
+iconPath = f"{__file__.replace("fw-fanctrl-indicator.py", '')}fan-white.svg" # use the location of the this python file to find the icons (because they should be installed there)
 
 print("---Starting fw-fanctrl-indicator---")
 print('configPath:', configPath)
@@ -20,44 +20,42 @@ def main():
     indicator = appindicator.Indicator.new("customtray", "computer-laptop-symbolic", appindicator.IndicatorCategory.APPLICATION_STATUS)
     strategyList, currentStrategy = buildStrategyList()
     indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
-    if os.path.exists(iconPath):
-        indicator.set_icon_full(iconPath, 'Custom Icon')
+    if os.path.exists(iconPath): # check if the icon exists
+        indicator.set_icon_full(iconPath, 'Custom Icon') # set the icon
     else:
-        print('\033[91m' + "Icon File not Found!" + '\033[0m' + " - Reverting to default computer-laptop-symbolic (see fw-fanctrl-indicator.py -h)") 
-        print("")
+        print('\033[91m' + "Icon File not Found!" + '\033[0m' + " - Reverting to default computer-laptop-symbolic")  # warn the user icon is not found, instead it will default to "computer-laptop-symbolic"
     indicator.set_menu(menu())
     gtk.main()
 
 def menu():
     menu = gtk.Menu()
-    for i in strategyList:
-        displayStrategy = ''
+    for i in strategyList: # read the strategyList created by buildStrategyList
         if i == currentStrategy: # this is to distinguish the current strategy
-            menu.append(gtk.SeparatorMenuItem())
-            command = gtk.MenuItem(label='[' + i + ']') 
+            menu.append(gtk.SeparatorMenuItem()) # create a menu separator before to distinguish the current strategy
+            command = gtk.MenuItem(label='[' + i + ']') # add brackets to the current strategy to distingish
             command.connect('activate', lambda _, s=i: strategyClick(s)) # use a lambda function to pass the current strategy into the function call
-            menu.append(command)
-            menu.append(gtk.SeparatorMenuItem())
-        else:
-            command = gtk.MenuItem(label=i)
+            menu.append(command) # add the command to the menu
+            menu.append(gtk.SeparatorMenuItem()) # create a menu separator after to distinguish the current strategy
+        else: # strategy is not the current strategy
+            command = gtk.MenuItem(label=i) # create the menu item
             command.connect('activate', lambda _, s=i: strategyClick(s)) # use a lambda function to pass the current strategy into the function call
-            menu.append(command)
-    exittray = gtk.MenuItem(label='Exit Tray')
-    exittray.connect('activate', quit)
-    menu.append(exittray)
+            menu.append(command) # append the menu item to the menu
+    exittray = gtk.MenuItem(label='Exit Tray') # create the exit tray command
+    exittray.connect('activate', quit) # connect it to our quit() function
+    menu.append(exittray) # add it to the menu
   
-    menu.show_all()
-    return menu
+    menu.show_all() # show everything
+    return menu # return the menu for GTK to read
 
 def updateMenu():
     print('updating menu')
     indicator.set_menu(menu())
     gtk.main()
 
-def buildStrategyList(): # make "strategyList", a list with all the strategies from the config file
-    strategyList = []
-    
-    config = ''
+def buildStrategyList(): # make a list with all the strategies from the config file
+    strategyList = [] # make sure these variables can be used throughout the function
+    config = '' # make sure these variables can be used throughout the function
+
     try:
         with open(configPath, 'r') as configFile:
             config = json.load(configFile)
@@ -65,15 +63,14 @@ def buildStrategyList(): # make "strategyList", a list with all the strategies f
             print('\033[91m' + 'Config File not Found!' + '\033[0m')
             exit()
     for i in config['strategies']:
-        strategyList.append(i)
+        strategyList.append(i) # add each strategy from the config file to strategyList
     global currentStrategy
-    currentStrategy = config['defaultStrategy']
+    currentStrategy = config['defaultStrategy'] # guess that the current strategy is the default, since we can't read from fw-fanctrl, just change the strategy
     return strategyList, currentStrategy
     
 def strategyClick(strategy):
-    
     print(f'fw-fanctrl {strategy}')
-    os.system(f'fw-fanctrl {strategy}')
+    os.system(f'fw-fanctrl {strategy}') # run fw-fanctrl with the strategy as the argument
     global currentStrategy
     currentStrategy = strategy
     updateMenu()
--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# declare colors
 red='\033[31m'
 blue='\033[34m'
 green='\033[32m'
@@ -9,25 +10,26 @@ if [ $USER == "root" ]; then
   return
 fi
 
-echo -e $blue'---Installing Dependencies---'
+echo -e $blue'---Installing Dependencies (for Arch Linux)---'
 echo -e '(fw-fanctrl still must already be installed)'$normal
 sudo pacman -S libappindicator-gtk3 python-gobject # install dependencies (assume fw-fanctrl is already installed)
-#read -p "Enter the path for your fw-fanctrl install - ex. /home/YOUR_USER/.config/fw-fanctrl: " configPath
+
 echo -e $blue"---Copying Over Files---"$normal
-configPath=/home/$USER/.config/fw-fanctrl
+configPath=/home/$USER/.config/fw-fanctrl # fw-fanctrl config should be installed in ~/.config/fw-fanctrl
 sudo cp fw-fanctrl-indicator.py *.svg $configPath # move our python and icon files to where fw-fanctrl is installed
-echo -e $blue"---Modifying .service File to Reflect Current User---"$normal
+
+echo -e $blue"---Modifying .service File to Reflect Current User---"$normal # we need to modify the .service file so systemd can find our .py file
 sedArg1="s%INDICATOR%${configPath}/fw-fanctrl-indicator.py%g"
 sedArg2="s%FW_FANCTRL%${configPath}%g"
 sed -i -e ${sedArg1} fw-fanctrl-indicator.service # fingers crossed
 sed -i -e ${sedArg2} fw-fanctrl-indicator.service # let's hope this works
-
 sudo cp fw-fanctrl-indicator.service /home/$USER/.config/systemd/user/ # copy our systemd service file to where the user's services are stored
+
 echo -e $blue"---Enabling systemd Service---\n"$normal
 systemctl --user enable fw-fanctrl-indicator.service # enable the service
 systemctl --user start fw-fanctrl-indicator.service # start the service
-loginctl enable-linger hunterd
+loginctl enable-linger hunterd # make sure the service starts at boot
 
 echo -e $green"fw-fanctrl-indicator installed, configured, and started! displaying status\n"$normal
 
-systemctl --user status fw-fanctrl-indicator.service
+systemctl --user status fw-fanctrl-indicator.service # indicate the status of the service
